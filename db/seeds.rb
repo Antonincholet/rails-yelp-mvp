266864c776b1f4d28872192cd94ea73733005a8d
antonin cholet <antonin@cholet-avocat.fr>
2020-05-27 18:09:43 +0200

    add routes review

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+'require faker'
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+Review.destroy_all
+
+puts "Creating restaurants..."
+33.times do
+  restaurant = { name: "#{Faker::Coffee.blend_name}",
+                address: "#{Faker::Address.full_address}",
+                phone_number: "#{Faker::PhoneNumber.cell_phone}",
+                category: [ "chinese", "italian", "japanese", "french", "belgian" ].sample }
+  valid_restau = Restaurant.create!(restaurant)
+  puts "Created #{valid_restau.name}"
+  3.times do
+    review = { content: "#{Faker::GreekPhilosophers.quote}",
+            rating: "#{Faker::Number.between(from: 0, to: 5)}",
+            restaurant: valid_restau}
+    valid_review = Review.create!(review)
+    puts "Created review : #{valid_review.content} || #{valid_review}"
+  end
+end
+puts "Finished!"
